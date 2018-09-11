79ade86eec351125e819371b2970ca403e20f995
aamir3753 <ars3753669@gmail.com>
2018-09-11 21:08:55 +0500

    Test

---
+++ script.js
@@ -0,0 +1,91 @@
+var submitForm = false;
+
+
+function isNullOrWhiteSpace(str) {
+    return (!str || str.length === 0 || /^\s*$/.test(str))
+}
+userEmail.onblur = userEmailIsVal;
+function userEmailIsVal() {
+
+    if (isNullOrWhiteSpace(userEmail.value)) {
+        errorUserEmail.style = "color:red;display:block";
+        errorUserEmail.innerText = "Email is required";
+        return false;
+    }
+    else {
+        errorUserEmail.style = "display:none;"
+        return true;
+    }
+}
+companyName.onblur = companyNameIsVal;
+function companyNameIsVal() {
+
+    if (isNullOrWhiteSpace(companyName.value)) {
+        errorCompanyName.style = "color:red;display:block";
+        errorCompanyName.innerText = "Company name is required";
+        return false;
+    }
+    else {
+        errorCompanyName.style = "display:none;"
+        return true;
+    }
+}
+password.onblur = passwordIsVal;
+function passwordIsVal() {
+    var passwordString = password.value;
+    if (isNullOrWhiteSpace(passwordString)) {
+        errorPassword.style = "color:red;display:block";
+        errorPassword.innerText = "Password is required";
+        return false;
+    }
+    else if (passwordString.length < 8) {
+        errorPassword.style = "color:red;display:block";
+        errorPassword.innerText = "Password length should be at least 8";
+        return false;
+    }
+    else {
+        errorPassword.style = "display:none;"
+        return true;
+    }
+}
+confirmPassword.onblur = confirmPasswordIsVal;
+function confirmPasswordIsVal() {
+    if (confirmPassword.value != password.value) {
+        errorConfirmPassword.style = "color:red;display:block";
+        errorConfirmPassword.innerText = "Password do not match";
+        return false;
+    }
+    else {
+        errorConfirmPassword.style = "display:none";
+        return true;
+    }
+}
+chooseState.onclick = function () {
+    if (chooseState.value == "") {
+        chooseState.style = "color:#8e8b8a;"
+    }
+    else {
+        chooseState.style = "color:black;"
+    }
+}
+chooseState.onblur = chooseStateIsVal;
+function chooseStateIsVal() {
+    if (isNullOrWhiteSpace(chooseState.value)) {
+        errorChooseState.style = "color:red;display:block";
+        errorChooseState.innerText = "Choose your state";
+        return false;
+    }
+    else {
+        errorChooseState.style = "display:none";
+        return true;
+    }
+}
+createAccount.onclick = function () {
+    if (userEmailIsVal() && companyNameIsVal() && passwordIsVal() && confirmPasswordIsVal() && chooseStateIsVal()) {
+        alert("Form submited");
+    }
+    else {
+        event.preventDefault();
+        console.log("Form is not submited");
+    }
+}
